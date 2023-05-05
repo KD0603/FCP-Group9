c924bf0ab960fa82ecabfaf52dc3d6d2d1a36f2b
samwing095 <131913354+samwing095@users.noreply.github.com>
2023-05-05 10:05:07 +0100

    Add files via upload

--- Task 2 Q1.py
+++ Task 2 Q1.py
@@ -8,15 +8,23 @@ Created on Tue May  2 00:02:41 2023
 import argparse
 import numpy as np
 
+==================================================================
+For 3×3 Sudoku puzzle: subgrid rows is 3 and subgrid columns is 3
+For 3×2 Sudoku puzzle: subgrid rows is 2 and subgrid columns is 3
+==================================================================
+
+#User will need to input some information of the sudoku
 row_sol = int(input('The row to solve:'))
 col_sol = int(input('The column to solve:'))
+
+#This paragraph explains why the code will need a --explain in the configuration to work 
 def parse_args():
     parser = argparse.ArgumentParser(description='Solve a Sudoku puzzle.')
     parser.add_argument('--explain', action='store_true', help='Provide step-by-step instructions for solving the Sudoku puzzle.')
     return parser.parse_args()
 
 def solve_sudoku(puzzle, explain=False, step=0):
-    # Find an empty cell
+# Find an empty cell
     for i in range(puzzle.shape[0]):
         for j in range(puzzle.shape[1]):
             if puzzle[i][j] == 0:
@@ -35,15 +43,13 @@ def solve_sudoku(puzzle, explain=False, step=0):
     return True
 
 def is_valid(puzzle, row, col, value):
-    # Check row
+ # Check if the numbers input by the user is valid, return if the number is not
     if value in puzzle[row]:
         return False
 
-    # Check column
     if value in puzzle[:,col]:
         return False
 
-    # Check subgrid
     subgrid_row = (row // row_sol) * row_sol
     subgrid_col = (col // col_sol) * col_sol
     subgrid = puzzle[subgrid_row:subgrid_row+row_sol, subgrid_col:subgrid_col+col_sol]
@@ -56,6 +62,9 @@ def main():
     args = parse_args()
 
     # Define the puzzle
+=============================================================================
+Please place the Sudoku Puzzle in the following area and keep the <np,array>.  
+=============================================================================
     puzzle = np.array([
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
